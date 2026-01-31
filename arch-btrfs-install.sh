59290733d31c66c4454b993d0555297dfac91820
Gabriel Millet <gabrielmlt@protonmail.ch>
2026-01-31 11:10:54 +0100

    fix: choose to partition or install core system

--- arch-btrfs-install.sh
+++ arch-btrfs-install.sh
@@ -4,80 +4,52 @@ set -eao pipefail
 
 timedatectl
 
-read -p "Wich disk would you like to partition (eg. nvme0n1,sda,sdb) ? " disk_name
+partition_disk() {
+  read -p "Wich disk would you like to partition (eg. nvme0n1,sda,sdb) ? " disk_name
+  fdisk /dev/${disk_name}
+  read -p "What's the name of the root partition (/) ?" root_part_name
+  mkfs.btrfs /dev/${root_part_name}
+  read -p "What's the name of the EFI partition (efi) ?" efi_part_name
+  mkfs.fat -F32 /dev/${efi_part_name}
+  mount /dev/${root_part_name} /mnt
+  btrfs subvolume create /mnt/@
+  btrfs subvolume create /mnt/@home
+  btrfs subvolume create /mnt/@var
+  umount /mnt
+  mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /mnt/${root_partition_name} /mnt
+  mkdir -p /mnt/{boot,home,var}
+  mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /mnt/${root_partition_name} /mnt
+  mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var /mnt/${root_partition_name} /mnt
+  mount /dev/${efi_part_name} /mnt/boot
+  reflector --sort rate --completion-percent 100 --age 12 --country France --protocol https --save /etc/pacman.d/mirrorlist
+  pacstrap -K /mnt base linux linux-firmware git vim sof-firmware intel-ucode
+  genfstab -U /mnt >> /mnt/etc/fstab
+  hwclock --systohc
+}
 
-fdisk /dev/${disk_name}
-
-read -p "What's the name of the root partition (/) ?" root_part_name
-
-mkfs.btrfs /dev/${root_part_name}
-
-read -p "What's the name of the EFI partition (efi) ?" efi_part_name
-
-mkfs.fat -F32 /dev/${efi_part_name}
-
-mount /dev/${root_part_name} /mnt
-
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@var
-
-umount /mnt
-
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /mnt/${root_partition_name} /mnt
-
-mkdir -p /mnt/{boot,home,var}
-
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /mnt/${root_partition_name} /mnt
-mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var /mnt/${root_partition_name} /mnt
-mount /dev/${efi_part_name} /mnt/boot
-
-reflector --sort rate --completion-percent 100 --age 12 --country France --protocol https --save /etc/pacman.d/mirrorlist
-
-pacstrap -K /mnt base linux linux-firmware git vim sof-firmware intel-ucode
-
-genfstab -U /mnt >> /mnt/etc/fstab
-hwclock --systohc
-
-arch-chroot /mnt
-
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-
-vim /etc/locale.gen
-
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-echo "KEYMAP=us" > /etc/vconsole.conf
-
-echo "archlinux" > /etc/hostname
-
-mkinitcpio -P
-
-passwd
-
-read -p "New user name: " new_username
-
-useradd -mG wheel ${new_username}
-
-passwd ${new_username}
-
-pacman -S refind
-
-refind-install
-
-echo "+--------------------------------+"
-echo "                                  "
-echo "Don't forget to configure refind !"
-echo "                                  "
-echo "+--------------------------------+"
-
-sleep 2
-
-pacman -S wofi hyprland nvidia-open ghostty
-
-mkdir -p /home/${new_username}/.config/hypr
-
-cat <<EOF >> /home/${new_username}/.config/hypr/hyprland.conf
+install_core() {
+  arch-chroot /mnt
+  ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+  vim /etc/locale.gen
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+  echo "KEYMAP=us" > /etc/vconsole.conf
+  echo "archlinux" > /etc/hostname
+  mkinitcpio -P
+  passwd
+  read -p "New user name: " new_username
+  useradd -mG wheel ${new_username}
+  passwd ${new_username}
+  pacman -S refind
+  refind-install
+  echo "+--------------------------------+"
+  echo "                                  "
+  echo "Don't forget to configure refind !"
+  echo "                                  "
+  echo "+--------------------------------+"
+  sleep 2
+  pacman -S wofi hyprland nvidia-open ghostty
+  mkdir -p /home/${new_username}/.config/hypr
+  cat <<EOF >> /home/${new_username}/.config/hypr/hyprland.conf
 monitor=eDP-1,disabled
 monitor=DP-1,disabled
 monitor=HDMI-A-1,1920x1080,auto,1
@@ -118,15 +90,34 @@ bind = \$mainMod SHIFT, 7, movetoworkspace, 7
 bind = \$mainMod SHIFT, 8, movetoworkspace, 8
 bind = \$mainMod SHIFT, 9, movetoworkspace, 9
 bind = \$mainMod SHIFT, 0, movetoworkspace, 10
-
 EOF
 
-pacman -S lightdm lightdm-gtk-greeter
+  pacman -S lightdm lightdm-gtk-greeter
 
-systemctl enable lightdm.service
+  systemctl enable lightdm.service
 
-echo "+---------------------------------+"
-echo "                                   "
-echo "Don't forget to configure lightdm !"
-echo "                                   "
-echo "+---------------------------------+"
+  echo "+---------------------------------+"
+  echo "                                   "
+  echo "Don't forget to configure lightdm !"
+  echo "                                   "
+  echo "+---------------------------------+"
+
+  echo "After configuring Refind and LightDM, you can safely un-chroot and umount -R /mnt then reboot."
+}
+
+while true; do
+  read -p "Want you to partition your disk or install the core system ? [partition/core] " install_part
+  case $install_part in
+    "partition")
+      partition_disk
+      break
+      ;;
+    "core")
+      install_core
+      break
+      ;;
+    *)
+      echo "Please enter one of the following : [partition] or [core]."
+      ;;
+    esac
+done
